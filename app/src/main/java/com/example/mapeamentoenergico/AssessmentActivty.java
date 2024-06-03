cd0506dcfa337f7e0cd83bc390e3ecf564a85503
victorchapiro <victorchapiro@gmail.com>
2024-06-02 22:31:58 -0300

    Ajustes finais para melhora do app

--- app/src/main/java/com/example/mapeamentoenergico/AssessmentActivty.java
+++ app/src/main/java/com/example/mapeamentoenergico/AssessmentActivty.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.TextView;
+
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -13,6 +15,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class AssessmentActivty extends AppCompatActivity {
     private RecyclerView recyclerView;
@@ -22,6 +25,21 @@ public class AssessmentActivty extends AppCompatActivity {
     private List<Appliance> checkedItems;
     private List<ApplianceAssessment> assessmentBill;
     private double conta;
+    private TextView textView;
+    String[] dicas = {
+            "Desligue aparelhos não utilizados: Desligue luzes, ventiladores, computadores e outros aparelhos quando não estiverem em uso.",
+            "Use lâmpadas LED: Elas consomem menos energia e duram mais.",
+            "Aproveite a luz natural: Mantenha as janelas abertas durante o dia para reduzir o uso de lâmpadas.",
+            "Regule o termostato: Ajuste o termostato para uma temperatura eficiente. No verão, mantenha-o mais alto; no inverno, mais baixo.",
+            "Desconecte carregadores: Desconecte carregadores de celular e outros dispositivos quando não estiverem em uso, pois continuam consumindo energia.",
+            "Utilize eletrodomésticos eficientes: Opte por aparelhos com selo de eficiência energética (Procel ou Energy Star).",
+            "Cozinhe de forma eficiente: Use tampas nas panelas e cozinhe em fogo baixo para economizar gás.",
+            "Faça manutenção regular: Limpe ou troque filtros de ar condicionado e aquecedores regularmente para garantir eficiência.",
+            "Evite stand-by: Desligue aparelhos da tomada em vez de deixá-los em modo stand-by.",
+            "Lave roupas com água fria: Sempre que possível, use água fria para lavar roupas e acumule uma quantidade maior antes de lavar.",
+            "Seque roupas ao ar livre: Sempre que possível, evite usar a secadora e seque as roupas ao ar livre.",
+            "Utilize timers: Use temporizadores em aparelhos como aquecedores de água e luzes externas."
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,6 +50,11 @@ public class AssessmentActivty extends AppCompatActivity {
         buttonUser = findViewById(R.id.buttonUser);
         toolbarHome = findViewById(R.id.toolbarHome);
         recyclerView = findViewById(R.id.recyclerView);
+        textView = findViewById(R.id.textView5);
+        Random random = new Random();
+        int indiceAleatorio = random.nextInt(dicas.length);
+        String dicaAleatoria = dicas[indiceAleatorio];
+        textView.setText(dicaAleatoria);
         setSupportActionBar(toolbarHome);
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
